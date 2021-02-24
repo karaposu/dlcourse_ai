685a16913df5de5435b7ff33e2d9fcb2cf07ffb9
Enes <31979644+karaposu@users.noreply.github.com>
2021-02-24 01:36:06 -0800

    Update knn.py

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -89,6 +89,7 @@ class KNN:
             
             v1=v1.reshape(1, v1.shape[0])
             tmp=self.train_X-v1
+            print("tmp[",i,"]",tmp)
             dists[i_test]=np.sum(np.abs(tmp),-1)
         return dists
 
@@ -141,7 +142,7 @@ class KNN:
         pred, np array of bool (num_test_samples) - binary predictions 
            for every test sample
         '''
-        
+        print("dists:",dists)
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
         for i in range(num_test):
